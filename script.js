d4ca95deaab937196bf0bd22e609c4ce98b2b7cf
Antti Sykäri <antti.sykari@gmail.com>
2013-10-10 18:42:39 +0300

    Call it RGB.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var ctx = document.querySelector('canvas').getContext('2d');
 ctx.fillRect(0, 0, 200, 200);
 
-function pixel(r, g, b, x, y) {
+function rgb(r, g, b, x, y) {
 	ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
 	ctx.fillRect(x, y, 1, 1);
 }
 
-pixel(255, 255, 255, 50, 50);
+rgb(255, 255, 255, 50, 50);
