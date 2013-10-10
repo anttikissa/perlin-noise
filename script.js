d3e9cd51f49d291737e0a7d2d76d0ffb4d2ffb7c
Antti Sykäri <antti.sykari@gmail.com>
2013-10-10 18:46:11 +0300

    Add `gray`.

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ function rgb(r, g, b, x, y) {
 	ctx.fillRect(x, y, 1, 1);
 }
 
+function gray(g, x, y) {
+	rgb(g, g, g, x, y);
+}
+
 rgb(255, 255, 255, 50, 50);
+
+gray(100, 0, 0);
+gray(200, 0, 1);
+gray(100, 1, 2);
+gray(240, 1, 3);
+gray(140, 2, 3);
