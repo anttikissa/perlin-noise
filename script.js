61bb9ba0b6543f7e964dc0d4343bc729c9861b3d
Antti Sykäri <antti.sykari@gmail.com>
2013-10-10 18:38:39 +0300

    Draw pixel.

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 var ctx = document.querySelector('canvas').getContext('2d');
-ctx.fillRect(10, 10, 180, 180);
+ctx.fillRect(0, 0, 200, 200);
+
+ctx.fillStyle = 'rgb(255, 255, 255)';
+ctx.fillRect(0, 0, 1, 1);
