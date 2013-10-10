75a8e65f7b82a4c506c42e3fbc30752c471c5d35
Antti Sykäri <antti.sykari@gmail.com>
2013-10-10 18:48:51 +0300

    Change scale.

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var ctx = document.querySelector('canvas').getContext('2d');
 ctx.fillRect(0, 0, 200, 200);
 
 function rgb(r, g, b, x, y) {
-	ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+	ctx.fillStyle = 'rgb('
+		+ Math.round(255 * r) + ', '
+		+ Math.round(255 * g) + ', '
+		+ Math.round(255 * b) + ')';
 	ctx.fillRect(x, y, 1, 1);
 }
 
@@ -10,10 +13,10 @@ function gray(g, x, y) {
 	rgb(g, g, g, x, y);
 }
 
-rgb(255, 255, 255, 50, 50);
+rgb(0, .9, .5, 50, 50);
 
-gray(100, 0, 0);
-gray(200, 0, 1);
-gray(100, 1, 2);
-gray(240, 1, 3);
-gray(140, 2, 3);
+gray(.1, 0, 0);
+gray(.2, 0, 1);
+gray(.3, 1, 2);
+gray(.4, 1, 3);
+gray(.5, 2, 3);
