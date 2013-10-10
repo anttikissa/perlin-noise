8618c4adfedd5b061d1fe16043f9ddd6aa04f48b
Antti Sykäri <antti.sykari@gmail.com>
2013-10-10 18:40:27 +0300

    Refactor into `pixel`.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var ctx = document.querySelector('canvas').getContext('2d');
 ctx.fillRect(0, 0, 200, 200);
 
-ctx.fillStyle = 'rgb(255, 255, 255)';
-ctx.fillRect(0, 0, 1, 1);
+function pixel(r, g, b, x, y) {
+	ctx.fillStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+	ctx.fillRect(x, y, 1, 1);
+}
+
+pixel(255, 255, 255, 50, 50);
